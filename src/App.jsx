d31774abab2d6737286880ab2b66bbe67ec7382a
tanisha1527 <vermatanisha620@gmail.com>
2024-12-01 23:22:08 +0530

    Replaced galleryimages and updated campus component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cover from './components/Cover/Cover'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/Campus/Campus'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Programs />
         <About/>
         <Title subTitle='Gallery' title='Campus photos' />
+        <Campus />
       </div>
     </div>
   )
--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Campus.css'
+import gallery_1 from '../../assets/gallery1.webp'
+import gallery_2 from '../../assets/gallery2.png'
+import gallery_3 from '../../assets/gallery3.png'
+import gallery_4 from '../../assets/gallery4.png'
+
 
 const Campus = () => {
   return (
     <div className='campus'>
-      
+       <div className="gallery">
+         <img src={gallery_1} alt="" />
+         <img src={gallery_2} alt="" />
+         <img src={gallery_3} alt="" />
+         <img src={gallery_4} alt="" />
+       </div>
     </div>
   )
 }
