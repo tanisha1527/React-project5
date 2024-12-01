930156b6f428b9620d5644f7dfabf0356167fa1e
tanisha1527 <vermatanisha620@gmail.com>
2024-12-02 00:47:11 +0530

    Add testimonials section with slider feature to next/back buttons

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,16 +1,96 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testimonials.css'
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
 import user_1 from '../../assets/user-1.png'
 import user_2 from '../../assets/user-2.png'
 import user_3 from '../../assets/user-3.png'
+import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
+
+  const slider = useRef();
+  let tx = 0;
+
+ const slideForward = ()=>{
+     if(tx > -50){
+       tx -= 25;
+     }
+     slider.current.style.transform = `translateX(${tx}%)`
+ }
+
+ const slideBackward = ()=>{
+  if(tx < 0){
+    tx += 25;
+  }
+  slider.current.style.transform = `translateX(${tx}%)`
+
+ }
+
   return (
     <div className='testimonials'>
-        <img src={next_icon} alt="" className='next-btn' />
-        <img src={back_icon} alt="" className='back-btn' />
+        <img src={next_icon} alt="" className='next-btn' onClick={slideForward} />
+        <img src={back_icon} alt="" className='back-btn' onClick={slideBackward} />
+        <div className="slider">
+          <ul ref={slider}>
+             <li>
+                <div className="slide">
+                   <div className="user-info">
+                      <img src={user_1} alt="" />
+                       <div>
+                           <h3>Sallena</h3>
+                           <span>Edusity, USA</span>
+                       </div>
+                   </div>
+                    <p> 
+                    Choosing to purse my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.
+                    </p>
+                </div>
+             </li>
+             <li>
+                <div className="slide">
+                   <div className="user-info">
+                      <img src={user_2} alt="" />
+                       <div>
+                           <h3>Marco Jackson</h3>
+                           <span>Edusity, USA</span>
+                       </div>
+                   </div>
+                    <p>
+                      Choosing to purse my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.
+                    </p>
+                </div>
+             </li>
+             <li>
+                <div className="slide">
+                   <div className="user-info">
+                      <img src={user_3} alt="" />
+                       <div>
+                           <h3>Angelina Davis</h3>
+                           <span>Edusity, USA</span>
+                       </div>
+                   </div>
+                    <p>
+                      Choosing to purse my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.
+                    </p>
+                </div>
+             </li>
+             <li>
+                <div className="slide">
+                   <div className="user-info">
+                      <img src={user_4} alt="" />
+                       <div>
+                           <h3>Vin Mathew</h3>
+                           <span>Edusity, USA</span>
+                       </div>
+                   </div>
+                    <p>
+                      Choosing to purse my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.
+                    </p>
+                </div>
+             </li>
+          </ul>
+        </div>
     </div>
   )
 }
