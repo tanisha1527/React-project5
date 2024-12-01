9220872d4c7f72a26e20adb4bc9711cb843b7ee2
tanisha1527 <vermatanisha620@gmail.com>
2024-12-01 23:34:17 +0530

    Added campus section

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Campus.css'
-import gallery_1 from '../../assets/gallery1.webp'
-import gallery_2 from '../../assets/gallery2.png'
+import gallery_1 from '../../assets/gallery1.png'
+import gallery_2 from '../../assets/gallery2.jpg'
 import gallery_3 from '../../assets/gallery3.png'
 import gallery_4 from '../../assets/gallery4.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 
 const Campus = () => {
@@ -15,6 +16,7 @@ const Campus = () => {
          <img src={gallery_3} alt="" />
          <img src={gallery_4} alt="" />
        </div>
+       <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
     </div>
   )
 }
