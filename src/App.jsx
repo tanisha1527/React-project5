25c21376b5c5797b1a605224c494de02ce5dab8c
tanisha1527 <vermatanisha620@gmail.com>
2024-12-01 16:04:54 +0530

    Added cover component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Cover from './components/Cover/Cover'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Cover />
     </div>
   )
 }
