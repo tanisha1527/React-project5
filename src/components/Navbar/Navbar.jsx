777bce29d7875f55263005e3f613dc7c79fe1699
tanisha1527 <vermatanisha620@gmail.com>
2024-12-02 20:04:18 +0530

    Add responsiveness to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -13,10 +14,15 @@ const Navbar = () => {
     })
  },[])
 
+ const [mobileMenu, setMobileMenu] = useState(false);
+ const toggleMenu = ()=>{
+    mobileMenu? setMobileMenu(false) : setMobileMenu(true);  
+ }
+
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
        <img src={logo} alt="" className='logo' />
-       <ul>
+       <ul className={mobileMenu?'':'hide-menu-icon'}>
           <li><Link to='cover' smooth={true} offset={0} duration={500}>Home</Link></li>
           <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
           <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
@@ -24,6 +30,7 @@ const Navbar = () => {
           <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
           <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
        </ul>
+       <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
     </nav>
   )
 }
