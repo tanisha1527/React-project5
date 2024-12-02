57c76df92287172d8ed3990160cd089bc1ac4df1
tanisha1527 <vermatanisha620@gmail.com>
2024-12-02 23:59:43 +0530

    Add videoPlayer component and video file in assets folder

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoPlayer.css'
+
+const VideoPlayer = () => {
+  return (
+    <div className='video-player'>
+        <video></video>
+    </div>
+  )
+}
+
+export default VideoPlayer
